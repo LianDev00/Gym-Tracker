350c6b4bf0f6a4a4c25d6c975be5f062bbdf344a
Antonio Cornejo <138829680+LianDev00@users.noreply.github.com>
2026-03-19 17:24:51 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,23 +1,15 @@
-const CACHE_NAME = 'heavy-duty-v1';
-const ASSETS = [
-  './index.html',
-  './manifest.json',
-  'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=DM+Sans:wght@300;400;500;600&display=swap'
-];
+const CACHE = 'gymtracker-v1';
+const ASSETS = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
   self.clients.claim();
 });
 
